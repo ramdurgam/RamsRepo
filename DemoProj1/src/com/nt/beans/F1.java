9013325f1c9a5af62022e2515d4122081603dd42
Dev2 <Dev2@gmail.com>
2021-06-13 17:10:08 +0530

    Dev1 Modified

--- DemoProj1/src/com/nt/beans/F1.java
+++ DemoProj1/src/com/nt/beans/F1.java
@@ -3,5 +3,5 @@ package com.nt.beans;
 public class F1 {
 public void x() {
 	System.out.println("F1.x()");
-}
+  }
 }
--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -1,16 +1,15 @@
 package com.nt.beans;
-
 import java.util.Date;
 
 public class Utility{
-
 	public Date getSysDate() {
 		return new Date();
-	}
-	public void m1() {
+}
+public void m1() {
 		System.out.println("Utility m1() method....12345");
-		System.out.println("abc13433");
-	
-	}
-
+	  	System.out.println("abc13433");
+     }
+public void m4() {
+	System.out.println("Utility.m4()");
+ }
 }
